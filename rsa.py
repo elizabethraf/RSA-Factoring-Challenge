ab8924d24dbea896b8242faa612e8c22fd83672e
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-03-14 00:29:01 -0700

    rsa.py

--- rsa.py
+++ rsa.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python3
-
 import sys
-import subprocess
 
-def check_factor(*args):
-    if len(args) != 3:
-        num2 = 1
+def is_prime(n):
+    """Checks whether a given number is prime."""
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
-    for a in args:
-    if isinstance(a, int):
-        num2 *= a
-        num1 = args[-1]
-    else:
-        num2 = args[-1]
-        num1 = args[-2]
+def find_prime_factors(n):
+    """Finds the prime factors of a given RSA number n."""
+    for i in range(2, n//2+1):
+        if n % i == 0 and is_prime(i) and is_prime(n//i):
+            return i, n//i
 
-    if num2 > num1:
-        num1, num2 = num2, num1
+if len(sys.argv) != 2:
+    print('Usage: python rsa.py <file>')
+    sys.exit(1)
 
-        num_str = f"{args[0]}={args[1]}*"
-        return f"{num_str}{num1}*{num2}"
-
-    if len(sys.argv) != 2:
-        print('Usage: rsa <file>')
-            sys.exit(1)
-    else:
-        with open(sys.argv[1], 'r') as f:
-        for line in f:
-            num = int(line.strip())
-            result = subprocess.check_output(['factor', str(num)])
-            factors = list(map(int, result.decode().strip().split(':')[1].split()))
-            print(check_factor(*factors))
+with open(sys.argv[1], 'r') as f:
+    for line in f:
+        n = int(line.strip())
+        p, q = find_prime_factors(n)
+        print(f"{n} = {p} * {q}")
 
